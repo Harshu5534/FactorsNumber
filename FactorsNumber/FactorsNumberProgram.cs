8d024e975ee9ac14825207b85726d0c53cde8982
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-13 09:23:02 +0530

    [Harshal]Add:Factor Number Using With Or Without Recursion

---
+++ FactorsNumber/FactorsNumberProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorsNumber
+{
+    public class FactorsNumberProgram
+    {
+        int factorial;
+        public void FactorsWithoutRecursion()
+        {
+            int Fact = 1;
+            Console.WriteLine("Enter The Number: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= Num; i++)
+            {
+                Fact = Fact * i;
+            }
+            Console.WriteLine("Factorial Number Is: " + Fact);
+        }
+        public int Factorial(int num)
+        {
+            //Console.WriteLine("Enter The Number: ");
+            //int Num = Convert.ToInt32(Console.ReadLine());
+            factorial =Factorial(num);
+            if (num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial(num - 1);
+            }
+        }
+
+    }      
+    
+}
+    
--- FactorsNumber/Program.cs
+++ FactorsNumber/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To The Program");
+﻿
+using FactorsNumber;
+FactorsNumberProgram factorsNumberProgram=new FactorsNumberProgram();
+factorsNumberProgram.FactorsWithoutRecursion();
+factorsNumberProgram.Factorial(5);
